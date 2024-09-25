14d4d41158f5be9acc8363ce688695ace0e18a04
Vineet Sharma <vineet460728@gmail.com>
2024-09-25 23:40:46 +0530

    adding diff.java to the repository

---
+++ diff.java
@@ -0,0 +1,9 @@
+class echo
+{
+    public static void main(String[] args) throws Exception
+    {
+        int Diff =0,a=7,b=5;
+        Diff= a-b;
+        System.out.println("The difference of"+a+" &"+b+"is"+Diff);
+            }
+    }
