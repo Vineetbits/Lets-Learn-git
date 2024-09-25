c49dbea2115e745a14682520e6ce0c4ae25bb9d1
Vineet Sharma <vineet460728@gmail.com>
2024-09-25 23:32:12 +0530

    initial commit

---
+++ sum.java
@@ -0,0 +1,8 @@
+class echo
+{
+    public static void main(String[] args) throws Exception
+    {
+        int Sum =0,a=3,b=5;
+        System.out.println("The sum of"+a+" and"+b+"is"+Sum);
+            }
+    }
