7b576d7ea53d7c324a53f6f5a2aeeaa244d148a5
Vineet Sharma <vineet460728@gmail.com>
2024-09-29 21:20:44 +0530

    Added Multiply.java

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+class echo
+{
+    public static void main(String[] args) throws Exception
+    {
+        int Multiply =1,a=3,b=5;
+        Multiply = a*b;
+        System.out.println("The multiplication of"+a+" &"+b+"is"+Multiply);
+            }
+    }
