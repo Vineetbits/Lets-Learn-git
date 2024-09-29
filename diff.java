790974dc01959b0f8d62fc8a6acb8b3946734253
Vineet Sharma <vineet460728@gmail.com>
2024-09-29 21:48:44 +0530

    changed the values

--- diff.java
+++ diff.java
@@ -2,7 +2,7 @@ class echo
 {
     public static void main(String[] args) throws Exception
     {
-        int Diff =0,a=7,b=5;
+        int Diff =0,a=7,b=3;
         Diff= a-b;
         System.out.println("The difference of"+a+" &"+b+"is"+Diff);
             }
